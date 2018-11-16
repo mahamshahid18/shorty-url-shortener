39163f5b068f30f7e2a44565c05872660a64bf68
Maham Shahid <mahum.shahid934@gmail.com>
2018-11-16 15:11:14 +0500

    Environment variables updated for production build

--- frontend/shorty/src/environments/environment.prod.ts
+++ frontend/shorty/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   backendBaseUrl: 'https://shorty-backend.herokuapp.com',
-  appBaseUrl: 'https://mahamshahid18.github.io/shorty-url-shortener'
+  appBaseUrl: 'http://shorty.surge.sh'
 };
